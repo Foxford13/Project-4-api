6aae56e7bfa084a59216e4193eef81f9df76aac4
Jakub Dziekan <jdziek89@gmail.com>
2017-07-26 17:58:08 +0100

    before i break it

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
     has_many :conversations, class_name: "Conversation", foreign_key: "item_id"
       mount_uploader :image, ImageUploader
+      validates :title, presence: :true
+      validates :short_description, presence: :true
+      validates :full_description, presence: :true
+      validates :super_type, presence: :true
+      validates :sub_type, presence: :true
+      validates :location, presence: :true
+      validates :price, presence: :true
+
 end
