b371539f65a113eb0885538d185a3899e70ac75f
Jakub Dziekan <jdziek89@gmail.com>
2017-08-22 14:04:12 +0100

    update cors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password validations: false
+  has_secure_password
   has_many :items
   has_many :messages
   mount_uploader :image, ImageUploader
@@ -7,13 +7,7 @@ class User < ApplicationRecord
   has_many :conversations_sent, class_name: "Conversation", foreign_key: "sender_id"
   has_many :conversations_received, class_name: "Conversation", foreign_key: "receiver_id"
   validates :username, presence: true
-
-  has_and_belongs_to_many :items_watched, class_name: "Item", join_table: "items_users"
-  validates :email, presence: true, uniqueness: true, unless: :oauth_login?
-  validates :password, presence: true, confirmation:true, unless: :oauth_login?
-
-  def oauth_login?
-    github_id.present? || facebook_id.present?
-  end
+  validates :email, uniqueness: true, presence: true
+    has_and_belongs_to_many :items_watched, class_name: "Item", join_table: "items_users"
 
 end
